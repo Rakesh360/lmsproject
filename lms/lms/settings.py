905dd536fe24cf6f2955b8752b65065574fbad96
boxabhi <abhijeetg40@gmail.com>
2021-07-01 13:31:07 +0530

    -

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'lms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lmsproject',
+        'USER': 'lmsproject',
+        'PASSWORD': 'LmsBhu0987',
+        'HOST': 'lmsdatabase.c8hpn1snsqxi.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
