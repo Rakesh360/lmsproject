09045cbfe53465d1eec021e80382023f210287f7
abhijeet <abhijeetg40@gmail.com>
2021-07-03 17:07:31 +0530

    updated txt

--- lms/commans.sh
+++ lms/commans.sh
@@ -1,6 +1,6 @@
 ssh -i "lmsproject.pem" ubuntu@ec2-13-232-235-27.ap-south-1.compute.amazonaws.com
 
-
+ubuntu@ec2-13-232-235-27.ap-south-1.compute.amazonaws.com
 psql --host=lmsdatabase.c8hpn1snsqxi.ap-south-1.rds.amazonaws.com --port=5432 --username=lmsproject --password=LmsBhu0987 --dbname=lmsproject 
 
 # FOR DUMPING DATA 
