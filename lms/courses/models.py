b93ff3e061e27eb65314657f3a45a4637a6b70d2
boxabhi <abhijeetg40@gmail.com>
2021-10-25 11:16:09 +0530

    done

--- lms/courses/models.py
+++ lms/courses/models.py
@@ -139,7 +139,7 @@ class GoLive(BaseModel):
     image = models.ImageField(upload_to = 'live' , null=True , blank = True)
     live_date_time = models.DateTimeField(default=datetime.now)
     live_date = models.CharField(max_length=100 , null = True , blank=True)
-    courses = models.ManyToManyField(CoursePackage )
+    courses = models.ManyToManyField(CoursePackage , null=True , blank=True)
 
 
 
